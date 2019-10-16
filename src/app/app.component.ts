2fcc61978e52f32549caa8d5e78517db64c06c44
morovat <morovatparisa@gmail.com>
2019-10-16 08:14:25 +0330

    first version project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {Observable} from 'rxjs';
+import {SettingsService} from '../services/settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'shopping';
+  receivedChildMessage: string;
+  lng: string = 'fa';
+  class:string='rtl';
+  getMessage(message: string) {
+    this.receivedChildMessage = message;
+  }
+  constructor(private translate: TranslateService,private settings: SettingsService) {
+    translate.setDefaultLang(this.lng);
+    translate.use(this.lng);
+  }
+  langChange(event:any){
+    this.translate.use(event.target.value);
+    if (event.target.value=='fa'){
+      this.class='rtl';
+    }else{
+      this.class='ltr';
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,80 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CanfigurationComponent } from './canfiguration/canfiguration.component';
+import { ReportsComponent } from './reports/reports.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { CatComponent } from './category/cat/cat.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { OrderComponent } from './orders/order/order.component';
+import {DpDatePickerModule} from 'ng2-jalali-date-picker';
+import { OrderfilterPipe } from './pip/orderfilter.pipe';
+import { ProductComponent } from './products/product/product.component';
+import {JalaliPipe} from './pip/jalali.pipe';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { SortPipe } from './pip/sort.pipe';
+import { NgSelect2Module } from 'ng-select2';
+const routes: Routes = [
+  // {    path: '', component: LoginComponent},
+  {    path: '', component: DashboardComponent},
+  {    path: 'orders', component: OrdersComponent},
+  {    path: 'order', component: OrderComponent},
+  {    path: 'order/:id', component: OrderComponent},
+  {    path: 'products', component: ProductsComponent},
+  {    path: 'category', component: CategoryComponent},
+  {    path: 'configuration', component: CanfigurationComponent},
+  {    path: 'reports', component: ReportsComponent},
+  {    path: 'cat', component: CatComponent},
+  {    path: 'cat/:id', component: CatComponent},
+  {    path: 'product', component: ProductComponent},
+  {    path: 'product/:id', component: ProductComponent},
+];
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CategoryComponent,
+    ProductsComponent,
+    OrdersComponent,
+    CanfigurationComponent,
+    ReportsComponent,
+    LoginComponent,
+    CatComponent,
+    OrderComponent,
+    OrderfilterPipe,
+    ProductComponent,
+    JalaliPipe,
+    JalaliPipe,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    AngularFontAwesomeModule,
+    FormsModule,
+    HttpClientModule,
+    DpDatePickerModule,
+    NgSelect2Module,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
